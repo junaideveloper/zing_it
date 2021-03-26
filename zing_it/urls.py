2832592258f5cc2c14369f1ec67708bf6bef4e47
Junaid TT <junaidkodoor@gmail.com>
2021-03-26 22:55:25 +0400

    playlist added

--- zing_it/urls.py
+++ zing_it/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import home,search,about
+from .views import home,playlist
 
 urlpatterns = [
 
     path('', home,name= 'home'),
-    path('search/', search, name='search'),
-    path('search/<data>/', search, name='search'),
-    path('about/', about, name='about')
+    path('playlist/<int:id>/', playlist, name='playlist')
+
 
 ]
